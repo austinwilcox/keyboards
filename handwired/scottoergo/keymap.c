089fe7a3957f48b4944988a4e6fb3980c31f055b
Austin Wilcox <austin21wilcox@gmail.com>
2025-04-17 15:32:19 -0600

    fix mapping so that + is on the num layer

--- handwired/scottoergo/keymap.c
+++ handwired/scottoergo/keymap.c
@@ -70,13 +70,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_NUM] = LAYOUT_split_3x5_3(
         KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_QUOT, KC_4,    KC_5,    KC_6,    KC_EQL,  KC_NO,   KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+        KC_QUOT, KC_4,    KC_5,    KC_6,    KC_PLUS,  KC_NO,   KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS, KC_NO,   KC_NO,   KC_NO,   KC_DOT,   KC_NO,
         KC_DOT, KC_MINS, KC_0, KC_NO,    KC_BSPC,   KC_NO
     ),
     [_SYM] = LAYOUT_split_3x5_3(
         KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_DQUO, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS, KC_NO,   KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+        KC_DQUO, KC_DLR,  KC_PERC, KC_CIRC, KC_EQL, KC_NO,   KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, KC_NO,   KC_NO,   KC_NO,   KC_DOT,   KC_NO,
         KC_LPRN, KC_UNDS, KC_RPRN, KC_NO,   KC_NO,   KC_NO
     ),
