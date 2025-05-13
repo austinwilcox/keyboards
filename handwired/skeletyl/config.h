438083fea4cb26c1cf8089ca9ad62cec01151049
Austin Wilcox <austin21wilcox@gmail.com>
2025-05-13 10:58:46 -0600

    start working on the firmware for skeletyl

---
+++ handwired/skeletyl/config.h
@@ -0,0 +1,38 @@
+//TODO: Add the dactyl manuform config information
+#pragma once
+#include "config_common.h"
+
+// USB Device descriptor parameter
+/* #define VENDOR_ID       0xFEED */
+/* #define PRODUCT_ID      0x6060 */
+/* #define DEVICE_VER      0x0001 */
+/* #define MANUFACTURER    Austin */
+/* #define PRODUCT         ScottoErgo */
+
+// Key matrix size
+/* #define MATRIX_ROWS 4 */
+/* #define MATRIX_COLS 10 */
+
+/* // Define the pin connections for rows and columns */
+/* #define MATRIX_ROW_PINS { D0, D1, D2, D3 } */
+/* #define MATRIX_COL_PINS { F7, B1, B3, B2, B6, C6, D7, E6, B4, B5} */
+
+/* // Set the diode direction */
+/* #define DIODE_DIRECTION COL2ROW */
+
+// Debounce time in milliseconds
+/* #define DEBOUNCE    5 */
+
+// Mechanical locking support. Use KC_LCAP, KC_LNUM, or KC_LSCR in keymap for these keys.
+/* #define LOCKING_SUPPORT_ENABLE */
+
+// Locking resynchronize hack
+/* #define LOCKING_RESYNC_ENABLE */
+
+// Bootmagic Lite key configuration
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
+
+#define TAPPING_TERM 200
+/* #define PERMISSIVE_HOLD */
+/* #define TAPPING_TERM_PER_KEY */
---
+++ handwired/skeletyl/rules.mk
@@ -0,0 +1,23 @@
+# MCU name
+MCU = atmega32u4
+TAP_DANCE_ENABLE = yes
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build options
+# Enable or disable features as needed
+BOOTMAGIC_ENABLE = lite    # Enable Bootmagic Lite for keycode programming
+# MOUSEKEY_ENABLE = no      # Disable mouse keys
+EXTRAKEY_ENABLE = yes     # Enable extra keys (system control, media control)
+CONSOLE_ENABLE = no       # Disable console for debugging
+COMMAND_ENABLE = no       # Disable command feature
+NKRO_ENABLE = yes         # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no     # Disable backlight
+RGBLIGHT_ENABLE = no      # Disable RGB lighting
+
+# AUDIO_SUPPORTED = no        # Audio is not supported
+
+# MOUSE_SHARED_EP = no # Unify multiple HID interfaces into a single Endpoint
+
+# SERIAL_DRIVER = usart
