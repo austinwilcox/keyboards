73bc455e0e9049c2bfcc77bfe0e8c28e9ad2eedf
Austin Wilcox <austin21wilcox@gmail.com>
2025-05-14 10:14:14 -0600

    building

--- handwired/skeletyl/rules.mk
+++ handwired/skeletyl/rules.mk
@@ -8,15 +8,12 @@ BOOTLOADER = caterina
 # Build options
 # Enable or disable features as needed
 # MOUSEKEY_ENABLE = no      # Disable mouse keys
-EXTRAKEY_ENABLE = yes     # Enable extra keys (system control, media control)
-CONSOLE_ENABLE = no       # Disable console for debugging
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+SPLIT_KEYBOARD = yes
 COMMAND_ENABLE = no       # Disable command feature
 NKRO_ENABLE = yes         # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no     # Disable backlight
 RGBLIGHT_ENABLE = no      # Disable RGB lighting
-
-# AUDIO_SUPPORTED = no        # Audio is not supported
-
-# MOUSE_SHARED_EP = no # Unify multiple HID interfaces into a single Endpoint
-
-# SERIAL_DRIVER = usart
