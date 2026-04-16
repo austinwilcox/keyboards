25ceed5ae3e23f99177121232dacd75822ad6fea
Austin Wilcox <austin21wilcox@gmail.com>
2026-04-16 09:45:31 -0600

    add scottfrog

--- handwired/void9/keymaps/vial/keymap.c
+++ handwired/void9/keymaps/vial/keymap.c
@@ -9,19 +9,19 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ortho_3x3(
-        KC_7, KC_8,    KC_9,
+        KC_1, KC_2,    MO(_FN1),
         KC_4, KC_5,    KC_6,
-        KC_1, KC_2,    MO(_FN1)
+        KC_7, KC_8,    KC_9
     ),
     [_FN1] = LAYOUT_ortho_3x3(
-        KC_MPRV, KC_MPLY, KC_MNXT,
+        _______, _______, MO(_FN2),
         KC_VOLD, KC_MUTE, KC_VOLU,
-        _______, _______, MO(_FN2)
+        KC_MPRV, KC_MPLY, KC_MNXT
     ),
     [_FN2] = LAYOUT_ortho_3x3(
-        KC_F1,   KC_F2,   KC_F3,
+        _______, _______, MO(_FN3),
         KC_F4,   KC_F5,   KC_F6,
-        _______, _______, MO(_FN3)
+        KC_F1,   KC_F2,   KC_F3
     ),
     [_FN3] = LAYOUT_ortho_3x3(
         _______, _______, _______,
